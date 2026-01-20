2fc590bf7cda32bc8828b7b9bc13c08e0b326025
bayo <bayodeog@gmail.com>
2026-01-20 14:55:35 +0100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Adebayode Adegoke on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
